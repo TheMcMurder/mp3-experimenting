216fd774ceda211a5a2b2bac1765beb6b243d887
the_mcmurder <3059715+TheMcMurder@users.noreply.github.com>
2022-07-01 16:51:53 -0600

    adding icons

--- src/spa/header/Header.jsx
+++ src/spa/header/Header.jsx
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-violet-900">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -33,16 +33,16 @@ export default function Header() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  {/* <img
+                  <img
                     className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
+                    src="../build/icons/icon.svg"
+                    alt="Audiobook metadata manager"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  /> */}
+                    src="../build/icons/icon.svg"
+                    alt="Audiobook metadata manager"
+                  />
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
